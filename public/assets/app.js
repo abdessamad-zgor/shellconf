3b1e510bb528031223cf7443d8ba07fba0df6186
Abdessamad Zgor <abdessamadzgor@gmail.com>
2024-06-10 23:05:37 +0100

    select prompt properly working

---
+++ public/assets/app.js
@@ -0,0 +1,7 @@
+function selectPrompt(e) {
+  let promptElements = document.querySelectorAll('.prompt')
+  for (let element of promptElements) {
+    element.classList.remove('border-sky-500')
+  }
+  if (e.target) e.target.classList.add('border-sky-500')
+}
